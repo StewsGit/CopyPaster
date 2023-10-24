3758de3610b5f4dcbc164d8f78fd6a2a0d59706d
Stuie <Stuie.vm@gmail.com>
2023-10-24 18:53:40 +0200

    Added Project

---
+++ Copy-Paster/Program - Copy.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+public class HotkeyManager
+{
+    [DllImport("user32.dll")]
+    public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+    [DllImport("user32.dll")]
+    public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+    public const int WM_HOTKEY = 0x0312;
+}
+
+public class ClipboardListener
+{
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+}
+
+public class YourForm : Form
+{
+    const int COPY_HOTKEY_ID = 1;
+    const int PASTE_HOTKEY_ID = 2;
+
+    private IntPtr hookId = IntPtr.Zero;
+
+    public YourForm()
+    {
+        hookId = SetHook(KeyboardHookCallback);
+        ClipboardListener.AddClipboardFormatListener(this.Handle);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            UnhookWindowsHookEx(hookId);
+            ClipboardListener.RemoveClipboardFormatListener(this.Handle);
+        }
+        base.Dispose(disposing);
+    }
+
+    private IntPtr SetHook(HookProc proc)
+    {
+        using (ProcessModule curModule = Process.GetCurrentProcess().MainModule)
+        {
+            return SetWindowsHookEx(13, proc, GetModuleHandle(curModule.ModuleName), 0);
+        }
+    }
+
+    private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    private IntPtr KeyboardHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0 && wParam == (IntPtr)0x0100) // WM_KEYDOWN
+        {
+            int vkCode = Marshal.ReadInt32(lParam);
+
+            if (ModifierKeys == Keys.Control)
+            {
+                if (vkCode == (int)Keys.C)
+                {
+                    // Handle Ctrl+C (copy) event
+                    string clipboardData = Clipboard.GetText();
+                    // Save the data to your storage (e.g., a list of slots)
+                }
+                else if (vkCode == (int)Keys.V)
+                {
+                    // Handle Ctrl+V (paste) event
+                    // Retrieve data from your storage and set it to the clipboard
+                    Clipboard.SetText("Pasted data here");
+                    // Simulate a Ctrl+V press
+                    SendKeys.Send("^(v)");
+                }
+            }
+        }
+
+        return CallNextHookEx(hookId, nCode, wParam, lParam);
+    }
+
+    protected override void WndProc(ref Message m)
+    {
+        if (m.Msg == HotkeyManager.WM_HOTKEY)
+        {
+            int id = m.WParam.ToInt32();
+
+            if (id == COPY_HOTKEY_ID)
+            {
+                // Capture and save clipboard data
+                string clipboardData = Clipboard.GetText();
+                // Save the data to your storage (e.g., a list of slots)
+            }
+            else if (id == PASTE_HOTKEY_ID)
+            {
+                // Retrieve and paste clipboard data
+                // Retrieve data from your storage and set it to the clipboard
+                Clipboard.SetText("Pasted data here");
+                // Simulate a Ctrl+V press
+                SendKeys.Send("^(v)");
+            }
+        }
+
+        base.WndProc(ref m);
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        // Register hotkeys when the form loads
+        HotkeyManager.RegisterHotKey(this.Handle, COPY_HOTKEY_ID, 0x0002 /*Ctrl*/, (uint)Keys.C);
+        HotkeyManager.RegisterHotKey(this.Handle, PASTE_HOTKEY_ID, 0x0002 /*Ctrl*/, (uint)Keys.V);
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        // Unregister hotkeys when the form is closing
+        HotkeyManager.UnregisterHotKey(this.Handle, COPY_HOTKEY_ID);
+        HotkeyManager.UnregisterHotKey(this.Handle, PASTE_HOTKEY_ID);
+    }
+
+    #region WinAPI Declarations
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    #endregion
+}
---
+++ Copy-Paster/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Copy_Paster
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Initialize your application class
+            CopyPaster copyPaster = new CopyPaster();
+
+            // Hook into the Clipboard's ClipboardChanged event
+            ClipboardMonitor.Start(copyPaster.OnClipboardChanged);
+            // Rest of your code remains unchanged
+            Application.Run(copyPaster);
+        }
+
+        public class CopyPaster : ApplicationContext
+        {
+            private NotifyIcon trayIcon;
+            private const int DISPLAY_TEXT_ID = 3;
+            private const int HISTORY_SIZE = 5;
+
+            private List<string> clipboardHistory = new List<string>();
+
+            public CopyPaster()
+            {
+                // Initialize Tray Icon
+                trayIcon = new NotifyIcon()
+                {
+                    Icon = new System.Drawing.Icon("AppIcon.ico"),
+                    ContextMenu = new ContextMenu(),
+                    Visible = true
+                };
+
+                // Add menu items for clipboard history
+                for (int i = 0; i < HISTORY_SIZE; i++)
+                {
+                    trayIcon.ContextMenu.MenuItems.Add($"History Slot {i + 1}", ShowHistoryItem);
+                }
+
+                // Add "Exit" menu item
+                trayIcon.ContextMenu.MenuItems.Add(new MenuItem("Exit", Exit));
+
+                // Register a global hotkey for copying
+                HotkeyManager.RegisterHotKey(IntPtr.Zero, DISPLAY_TEXT_ID, 0x0003 /*Ctrl+Alt*/, (uint)Keys.C);
+
+            }
+
+            void ShowHistoryItem(object sender, EventArgs e)
+            {
+                // Retrieve the index of the clicked history item
+                int index = trayIcon.ContextMenu.MenuItems.IndexOf((MenuItem)sender);
+
+                // Display the corresponding history entry
+                if (index < clipboardHistory.Count)
+                {
+                    MessageBox.Show($"History Slot {index + 1}: {clipboardHistory[index]}", "Clipboard History");
+                }
+            }
+
+            void Exit(object sender, EventArgs e)
+            {
+                // Unregister the hotkey, hide tray icon, and stop monitoring clipboard
+                HotkeyManager.UnregisterHotKey(IntPtr.Zero, DISPLAY_TEXT_ID);
+                trayIcon.Visible = false;
+                ClipboardMonitor.Stop();
+
+                // Exit the application
+                Application.Exit();
+            }
+
+            public void OnClipboardChanged(object sender, EventArgs e)
+            {
+                // Handle clipboard changes here
+                string clipboardData = Clipboard.GetText();
+                Console.WriteLine("Clipboard Content Changed: " + clipboardData);
+
+                // Add the new entry to the clipboard history
+                clipboardHistory.Add(clipboardData);
+
+                // Remove the oldest entry if the history size exceeds the limit
+                if (clipboardHistory.Count > HISTORY_SIZE)
+                {
+                    clipboardHistory.RemoveAt(0);
+                }
+
+                // Update the context menu with the latest clipboard history
+                UpdateContextMenu();
+            }
+
+            private void UpdateContextMenu()
+            {
+                // Update the context menu items with clipboard history
+                for (int i = 0; i < HISTORY_SIZE; i++)
+                {
+                    if (i < clipboardHistory.Count)
+                    {
+                        // Update the text of the menu item
+                        trayIcon.ContextMenu.MenuItems[i].Text = $"History Slot {i + 1}: {clipboardHistory[i]}";
+                    }
+                    else
+                    {
+                        // If no history entry, clear the text
+                        trayIcon.ContextMenu.MenuItems[i].Text = $"History Slot {i + 1}";
+                    }
+                }
+            }
+        }
+
+        public class HotkeyManager
+        {
+            [DllImport("user32.dll")]
+            public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+            [DllImport("user32.dll")]
+            public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        }
+
+        // Helper class to monitor clipboard changes
+        public class ClipboardMonitor
+        {
+            public delegate void ClipboardChangedEventHandler(object sender, EventArgs e);
+            public static event ClipboardChangedEventHandler ClipboardChanged;
+
+            private static IntPtr nextClipboardViewer;
+
+            // Windows messages constants
+            private const int WM_DRAWCLIPBOARD = 0x0308;
+            private const int WM_CHANGECBCHAIN = 0x030D;
+
+            [DllImport("User32.dll")]
+            protected static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+
+            [DllImport("User32.dll", CharSet = CharSet.Auto)]
+            public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+            public static void Start(ClipboardChangedEventHandler clipboardChangedHandler)
+            {
+                // Register the event handler
+                ClipboardChanged += clipboardChangedHandler;
+
+                // Start monitoring the clipboard
+                nextClipboardViewer = SetClipboardViewer(IntPtr.Zero);
+            }
+
+            public static void Stop()
+            {
+                // Unregister the event handler
+                ClipboardChanged = null;
+
+                // Stop monitoring the clipboard
+                ChangeClipboardChain(nextClipboardViewer, IntPtr.Zero);
+            }
+
+            public static void OnClipboardChanged()
+            {
+                // Invoke the event when the clipboard changes
+                ClipboardChanged?.Invoke(null, EventArgs.Empty);
+            }
+
+            // Handle the WM_DRAWCLIPBOARD message to detect clipboard changes
+            public static IntPtr HandleClipboardMessage(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+            {
+                if (msg == WM_DRAWCLIPBOARD)
+                {
+                    // Clipboard content has changed
+                    OnClipboardChanged();
+                    handled = true;
+                }
+                else if (msg == WM_CHANGECBCHAIN)
+                {
+                    // The clipboard chain is changing
+                    if (wParam == nextClipboardViewer)
+                    {
+                        // If the next viewer in the chain is closing, repair the chain
+                        nextClipboardViewer = lParam;
+                    }
+                    else if (nextClipboardViewer != IntPtr.Zero)
+                    {
+                        // Forward the message to the next viewer in the chain
+                        SendMessage(nextClipboardViewer, msg, wParam, lParam);
+                    }
+                    handled = true;
+                }
+
+                return IntPtr.Zero;
+            }
+        }
+    }
+}
